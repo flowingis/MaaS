c818f90aace2f85a2597ed04f9d5b086da730795
Nicole Bartolini <bartolini.nicole@gmail.com>
2015-11-25 22:13:40 +0100

    Heroku works

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-var hostname = 'http://localhost:8088';
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.end('Ciao');
 });
 
-app.get('/memes', function(req, res){
+app.get('/memes', function (req, res) {
     var images = [];
+    var hostname = 'http://' + req.headers.host;
 
-    fs.readdir(__dirname+'/images/', function(err, files){
-        if(err) {
+    fs.readdir(__dirname + '/images/', function (err, files) {
+        if (err) {
             console.log(err);
         }
 
-        files.forEach(function(f){
-            var name =  f.replace('.png', '');
+        files.forEach(function (f) {
+            var name = f.replace('.png', '');
 
             images.push({
                 name: name,
-                url: hostname+'/meme/'+name
+                url: hostname + '/meme/' + name
             })
         });
 
@@ -33,11 +33,11 @@ app.get('/memes', function(req, res){
     });
 });
 
-app.get('/meme/:name', function(req, res){
-    var filename = __dirname+'/images/'+req.params.name+'.png';
+app.get('/meme/:name', function (req, res) {
+    var filename = __dirname + '/images/' + req.params.name + '.png';
     try {
         var img = fs.readFileSync(filename);
-    } catch(err) {
+    } catch (err) {
         res.writeHead(200, {"Content-Type": "application/json"});
         res.end(JSON.stringify({error: 'File not found'}));
     }
@@ -46,6 +46,6 @@ app.get('/meme/:name', function(req, res){
     res.end(img, 'binary');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
